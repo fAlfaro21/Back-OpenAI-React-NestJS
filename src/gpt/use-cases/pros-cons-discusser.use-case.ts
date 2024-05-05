2467db3a98bcf5bfc91ed2b6f92f8cbf6f95120c
Fernando <alfarogr@hotmail.com>
2024-05-05 15:02:05 +0200

    Corrects pros-cons-discusser.use-case in order to return the right value

--- src/gpt/use-cases/pros-cons-discusser.use-case.ts
+++ src/gpt/use-cases/pros-cons-discusser.use-case.ts
@@ -8,20 +8,19 @@ export const prosConsDiscusserUseCase = async( openai: OpenAI, options: Options 
     const { prompt } = options;
 
     const response = await openai.chat.completions.create({
+        model: 'gpt-4',
         messages: [
             { 
                 role: 'system', 
                 content: `
                 Se te dará una pregunta y tu tarea es dar una respuesta con pros y contras, la respuesta debe de ser en formato markdown, los pros y contras deben de estar en una lista,
-                `,
+                `
             },
             {
                 role: 'user', 
                 content: prompt,
             }
     ],
-        model: "gpt-4",
-
         //Estos son otros parámetros que nos ayudan a controlar las respuestas de la AI
         temperature: 0.8,
         max_tokens: 500,
@@ -29,6 +28,6 @@ export const prosConsDiscusserUseCase = async( openai: OpenAI, options: Options 
 
     //console.log(completion);
         
-    return response.choices[0].message.content;
+    return response.choices[0].message;
 
 }
